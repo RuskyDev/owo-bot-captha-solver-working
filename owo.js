c6f8383ee2af5149d5baadcf0f1ca4689438bdd3
RuskyDev <91284372+RuskyDev@users.noreply.github.com>
2023-07-03 23:33:16 +0500

    Update owo.js

--- owo.js
+++ owo.js
@@ -1,55 +1,55 @@
-const image2base64 = require('image-to-base64'),
-Anticaptcha = require('anticaptcha2'),
-anticaptcha = new Anticaptcha('api-key'),
-ImageToTextTask = Anticaptcha.ImageToTextTask,
-discord = require('discord.js'),
-client = new discord.Client(),
-sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+const imageToBase64 = require('image-to-base64');
+const Anticaptcha = require('anticaptcha2');
+const discord = require('discord.js-selfbot');
 
+const anticaptcha = new Anticaptcha('ENTER-YOUR-ANTICAPTCHA-KEY-HERE');
+const client = new discord.Client();
+
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
 client.on("ready", async () => {
-  var balance = await anticaptcha.getBalance();
-  console.log(`${client.user.tag}, Bring it on!\n${balance}`);
+  const balance = await anticaptcha.getBalance();
+  console.log(`${client.user.tag}, Bring it on!\nCredits: ${balance}`);
 });
 
-client.on("message", async (msg) => {
-  if (msg.author.id === "408785106942164992") {
-    if (msg.content.includes("Are you a real human?")) {
-      var link,
-      mAuthor = msg.author.lastMessageID,
-      mid = msg.channel.messages.get(`${mAuthor}`),
-      gAttachment = mid.attachments.map(file => file.id),
-      attachment_id = mid.attachments.get(`${gAttachment}`),
-      attached = `${attachment_id.url}`,
-      link = attached;
-      base64 = await image2base64(attached),
-      captha = new ImageToTextTask({
-        body: base64,
-        phrase: false,
-        case: true,
-        numeric: 0,
-        math: false,
-        length: [5,5]
-      }),
-      selesai = await anticaptcha.solve(captha);
-      msg.channel.send(selesai.text)
-        .then(() => {
-              msg.channel.awaitMessages(response => response.content.includes("I have verified that you are human"), {
-                max: 1,
-                time: 30000,
-                errors: ['time'],
-              })
-            .then((collected) => {
-                console.log("Success");
-                })
-              .catch(() => {
-                console.log("Failed");
-              });
+client.on("message", async (message) => {
+  if (message.author.id === "408785106942164992") {
+    if (message.content.includes("Are you a real human?")) {
+      const lastMessageId = message.author.lastMessageID;
+      const lastMessage = await message.channel.messages.fetch(lastMessageId);
+      const attachments = lastMessage.attachments.array();
+      
+      if (attachments.length > 0) {
+        const attachmentUrl = attachments[0].url;
+        const base64 = await imageToBase64(attachmentUrl);
+        const captcha = new Anticaptcha.ImageToTextTask({
+          body: base64,
+          phrase: false,
+          case: true,
+          numeric: 0,
+          math: false,
+          length: [5, 5]
+        });
+
+        const solvedCaptcha = await anticaptcha.solve(captcha);
+        
+        message.channel.send(solvedCaptcha.text)
+          .then(() => {
+            message.channel.awaitMessages(response => response.content.includes("I have verified that you are human"), {
+              max: 1,
+              time: 30000,
+              errors: ['time'],
+            })
+            .then(() => {
+              console.log("Success");
+            })
+            .catch(() => {
+              console.log("Failed");
+            });
           });
-    };
-  };
+      }
+    }
+  }
 });
 
-
-
-client.login("dicsordasdawdawdawd");
+client.login("ENTER-YOUR-DISCORD-TOKEN");
